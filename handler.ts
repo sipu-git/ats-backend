ea0b7889a301e2cf7764f64496fe4710f3d3afd0
Sipu-982 <sipurana38@gmail.com>
2026-02-06 22:20:00 +0530

    handler.ts file changed

--- handler.ts
+++ handler.ts
@@ -1,4 +1,23 @@
 import serverless from "serverless-http";
-import app from './app'
+import type { APIGatewayProxyEventV2, Context } from "aws-lambda";
+import app from "./app";
+import type { Request } from "express";
 
-export const lambdaHandler = serverless(app);
+const serverlessHandler = serverless(app, {
+  request: (req:Request, event: APIGatewayProxyEventV2) => {
+    if (event.rawPath && event.requestContext?.stage) {
+      const stagePrefix = `/${event.requestContext.stage}`;
+      if (event.rawPath.startsWith(stagePrefix)) {
+        req.url = event.rawPath.replace(stagePrefix, "") || "/";
+      }
+    }
+  }
+});
+
+export const lambdaHandler = async (
+  event: APIGatewayProxyEventV2,
+  context: Context
+) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  return serverlessHandler(event, context);
+};
