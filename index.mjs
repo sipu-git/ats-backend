86d4a4a6ef23c355b7729cdca5fbae10a50ee618
Sipu-982 <sipurana38@gmail.com>
2026-02-06 11:54:21 +0530

    index.ts file deleted

--- index.mjs
+++ index.mjs
@@ -1,14 +1,40 @@
-export const handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    },
-    body: JSON.stringify({
-      message: "Lambda deployed successfully 🚀",
-      source: "index.mjs",
-      timestamp: new Date().toISOString()
-    })
-  };
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import serverless from "serverless-http";
+import DocRoutes from "./routes/career.routes";
+import adminRoutes from "./routes/admin.routes";
+import { connectDB } from "./configs/db.config";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use("/api/career", DocRoutes);
+app.use("/api/admin", adminRoutes);
+
+app.get("/health", (_, res) => {
+  res.json({ status: "OK" });
+});
+
+let dbConnected = false;
+async function initDB() {
+  if (!dbConnected) {
+    await connectDB();
+    dbConnected = true;
+  }
+}
+
+export const handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  await initDB();
+  return serverless(app)(event, context);
 };
